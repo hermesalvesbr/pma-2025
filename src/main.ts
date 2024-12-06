ddefbc4e4f8aa6cfcf1817a769d9491a65ea450f
hermesalvesbr <hermes.alves@araripina.com.br>
2024-12-06 08:59:41 -0300

    refatora dependências e melhora tratamento de erros no roteador

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,15 @@
 // Plugins
 import { registerPlugins } from '@/plugins'
 
-// Components
-import App from './App.vue'
-
+import { createHead } from '@unhead/vue'
 // Composables
 import { createApp } from 'vue'
 
+// Components
+import App from './App.vue'
+
 const app = createApp(App)
-
+const head = createHead()
 registerPlugins(app)
-
+app.use(head)
 app.mount('#app')
